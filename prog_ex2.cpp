184457e1c15baaf092f41001408c08bc712d2324
Maya Raskin <mayaraskin14@gmail.com>
2023-05-26 16:38:58 +0300

    Added some basic main

--- prog_ex2.cpp
+++ prog_ex2.cpp
@@ -1,20 +1,19 @@
-﻿// prog_ex2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include "Heap.h"
 
-#include <iostream>
+int main() {
+    Heap max_heap = Heap(true);
+    Heap min_heap = Heap(false);
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    max_heap.setSecondHeap(&min_heap);
+    min_heap.setSecondHeap(&max_heap);
+
+    max_heap.Insert(3, "Eliya");
+    max_heap.Insert(1, "Dani");
+    max_heap.Insert(2, "Maya");
+    
+    max_heap.Delete(1);
+
+    for (int i = 0; i < max_heap.getSize(); i++)
+        std::cout << max_heap.getHeap()[i].data << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
